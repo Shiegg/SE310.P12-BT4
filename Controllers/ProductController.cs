20795c107fd16fba7935e4670db7630a843d973e
Shieng <22521312@gm.uit.edu.vn>
2024-10-10 18:10:58 +0700

    Up

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebsiteQuanLySanPham.Models;
+
+namespace WebsiteQuanLySanPham.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            QuanLySanPhamDataContext context = new QuanLySanPhamDataContext(ConfigurationManager.ConnectionStrings["QuanLySanPhamConnectionString"].ConnectionString);
+
+            List<Product> dsProduct = context.Products.ToList();
+
+            return View(dsProduct);
+        }
+    }
+}
